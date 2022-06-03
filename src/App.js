3db7fcffd04c368bf7ac3d238279c52ec3deb477
vamsi krishna <iteanzvamsi@titan.co.in>
2022-06-03 12:45:05 +0530

    new commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState,useEffect} from "react"
+import axios from "axios"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const[users,setUsers]=useState([])
+  const getUsers=async()=>{
+    const resposne=await axios.get("http://localhost:4000/users",{headers:{
+      "Content-Type": "application/json",
+    }})
+    console.log(resposne,"vamsii")
+    if(resposne.status===200){
+      console.log(resposne.data,"vamsi")
+      setUsers(resposne.data.users)
+    }
+  }
+  useEffect(()=>{
+    getUsers()
+  },[])
+  return (<center>
+    {users.length===0&&<h1>Loading....</h1>}
+    {users.length>0&&users.map((user)=><h1 key={user.id}>{user.name}</h1>)}
+  </center>
+   
   );
 }
 
